8389295227097b1101dfd70934f29490efb62f20
Quentin RA <quentin.ramsamy.cf@gmail.com>
2024-01-09 14:24:52 +0100

    Version 0.5.0 - init

--- jobs/fav.py
+++ jobs/fav.py
@@ -1,5 +1,21 @@
 
-hard_coded_commands = {}
+hard_coded_commands = {
+    "nmap" : [
+        "sudo nmap -Pn -p- -min-rate 5000 ${IP} -v",
+        "sudo nmap -Pn -sU -p- --min-rate 10000 ${IP} -v",
+        "rustscan -a ${IP} -- -sVC"
+    ],
+    "gobuster": [
+        "gobuster dir -u ${IP} -w /usr/share/wordlists/dirb/common.txt -t 64",
+        "gobuster dir -u ${IP} -w /usr/share/seclists/Discovery/Web-Content/quickhits.txt -t 64",
+        "gobuster dir -u ${IP} -w /usr/share/wordlists/dirbuster/directory-list-2.3-small.txt -t 64",
+        "gobuster dir -u ${IP} -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -t 64",
+    ],
+    "smb": [
+        "smbclient -L ${IP} -U Anonymous -N"
+        "smbclient -L ${IP} -U ${USER} -N"
+    ]
+}
 
 
 def do_job(args):
