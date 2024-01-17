de68b7cb516c80e6f65026893a9429d4e6814848
Quentin RA <quentin.ramsamy.cf@gmail.com>
2024-01-17 22:32:42 +0100

    feat: extract inject_word

--- jobs/request.py
+++ jobs/request.py
@@ -7,10 +7,10 @@ import html2text
 import urllib.parse
 
 import jobs.utils.tampering
-from impl.core import BaseProgramData
+import impl.core
 
 
-def run(parser : argparse.ArgumentParser, request_parser : argparse.ArgumentParser):
+def run(parser: argparse.ArgumentParser, request_parser: argparse.ArgumentParser):
     http_options = request_parser.add_argument_group("HTTP OPTIONS")
     payload_options = request_parser.add_argument_group("PAYLOAD OPTIONS")
     general_options = request_parser.add_argument_group("GENERAL OPTIONS")
@@ -27,8 +27,9 @@ def run(parser : argparse.ArgumentParser, request_parser : argparse.ArgumentPars
     injecter.add_argument("-I", dest="inject_file", help="Unencoded file to inject in parameter")
 
     http_options.add_argument("-X", dest="method", default="GET", help="HTTP Method (default=%(default)s)")
-    http_options.add_argument("-H", metavar="header", dest="headers", action="append", help="Header 'Name: Value', separated by colon. Multiple -H flags are accepted.")
-    http_options.add_argument("-d", dest="data", help="POST data.")
+    http_options.add_argument("-H", metavar="header", dest="headers", action="append",
+                              help="Header 'Name: Value', separated by colon. Multiple -H flags are accepted.")
+    http_options.add_argument("-d", dest="body", help="Request body data.")
 
     # PAYLOAD Options
     payload_options.add_argument("--tamper", dest="tamper", default="aliases",
@@ -37,91 +38,21 @@ def run(parser : argparse.ArgumentParser, request_parser : argparse.ArgumentPars
 
     # General Options
     general_options.add_argument("--raw", dest="is_raw", action="store_true", help="Raw HTML output")
-    general_options.add_argument("--nr", "--no-redirect", action="store_true", help="Don't follow the response redirection.")
-    general_options.add_argument('-t', metavar='threads', dest='threads', default=10, help='Number of threads (default=%(default)s).')
+    general_options.add_argument("--nr", "--no-redirect", action="store_true",
+                                 help="Don't follow the response redirection.")
+    general_options.add_argument('-t', metavar='threads', dest='threads', default=10,
+                                 help='Number of threads (default=%(default)s).')
     general_options.add_argument("-v", dest="is_verbose", action="store_true", help="Verbose output")
 
-    args = parser.parse_args()
-    args = verify_arguments(args)
-    do_job(args, args.inject)
-
-
-class RequestProgramData(BaseProgramData):
-    def __init__(self, args):
-        super().__init__(args)
-    
-        self.param = args.param
-        self.param_json = args.param_json
-        self.method = args.method
-        self.is_verbose = args.is_verbose
-        self.is_raw = args.is_raw
-        self.allow_redirects = not args.nr
-
-        if args.inject is not None:
-            self.payload = [args.inject]
-        elif args.inject_file is not None:
-            with open(args.inject_file, 'r') as f:
-                self.payload = ['\n'.join(f.readlines())]
-        else:
-            pass
-
-        self.tamper = jobs.utils.tampering.TamperingHandler(args.tamper)
-
-
-def verify_arguments(args):
-    data = ProgramData(args)
-    # compute URL
-    if args.url.startswith("http"):
-        data.url = args.url
-    else:
-        # noinspection HttpUrlsUsage
-        data.url = "http://" + args.url
-
-    if data.method == "GET":
-        if data.param_json:
-            print(f"[ERROR] Cannot use '-X GET' with '--json'.")
-            sys.exit(2)
-        data.parsed_url = urllib.parse.urlparse(data.url)
-        data.query_params = urllib.parse.parse_qs(data.parsed_url.query)
-        if data.param not in data.query_params:
-            data.query_params[data.param] = []
-    else:
-        data.parsed_url = data.url
-
-    # computer headers
-    data.headers = {}
-    for header in args.headers or []:
-        parts = header.split(":")
-        data.headers[parts[0].strip()] = parts[1].strip()
-
-    if args.data:
-        data.data = {k: v for k, v in [pair.split('=') for pair in args.data.split('&')]}
-    else:
-        data.data = {}
-
-    return data
+    args = RequestProgramData(parser.parse_args())
+    do_job(args, args.payload)
 
 
 def do_job(args, word):
-    word = args.tamper.apply(word)
-    body_data = args.data
-    json_data = None
-    updated_url = args.parsed_url
-    if args.method == "GET":
-        pu = args.parsed_url
-        args.query_params[args.param] = [word]
-        updated_query = urllib.parse.urlencode(args.query_params, doseq=True)
-        updated_url = urllib.parse.urlunparse((pu.scheme, pu.netloc, pu.path, pu.params, updated_query, pu.fragment))
-    else:
-        if args.param_json:
-            json_data = json.loads(word)
-        else:
-            body_data[args.param] = word
-
+    (url, body_data, json_data) = args.inject_word(word)
     try:
-        response = requests.request(args.method, updated_url, data=body_data, headers=args.headers,
+        response = requests.request(args.method, url, data=body_data, headers=args.headers,
                                     allow_redirects=args.allow_redirects, json=json_data)
-
         content = response.text
         if not args.is_raw:
             content = html2text.html2text(content)
@@ -144,3 +75,57 @@ def do_job(args, word):
                 print(content.replace("\n\n", "\n"))
     except Exception as e:
         print(f'[X] {e}')
+
+
+class RequestProgramData(impl.core.HttpProgramData):
+    def __init__(self, args):
+        super().__init__(args)
+
+        self.param = args.param
+        self.param_json = args.param_json
+
+        if args.inject is not None:
+            self.payload = [args.inject]
+        elif args.inject_file is not None:
+            with open(args.inject_file, 'r') as f:
+                self.payload = ['\n'.join(f.readlines())]
+        else:
+            # todo: ...
+            pass
+
+        self.tamper = jobs.utils.tampering.TamperingHandler(args.tamper)
+
+        if self.method == "GET":
+            if self.param_json:
+                print(f"[ERROR] Cannot use '-X GET' with '--json'.")
+                sys.exit(2)
+            self.parsed_url = urllib.parse.urlparse(self.url)
+            self.query_params = urllib.parse.parse_qs(self.parsed_url.query)
+            if self.param not in self.query_params:
+                self.query_params[self.param] = []
+        else:
+            self.parsed_url = self.url
+
+    def inject_word(self, word):
+        word = self.tamper.apply(word)
+        body_data = self.body
+        json_data = None
+        updated_url = self.parsed_url
+        if self.method == "GET":
+            pu = self.parsed_url
+            self.query_params[self.param] = [word]
+            updated_query = urllib.parse.urlencode(self.query_params, doseq=True)
+            updated_url = urllib.parse.urlunparse(
+                (pu.scheme, pu.netloc, pu.path, pu.params, updated_query, pu.fragment))
+        else:
+            if self.param_json:
+                json_data = json.loads(word)
+            else:
+                body_data[self.param] = word
+
+        return updated_url, body_data, json_data
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(" \
+               f"{super().__str__()}" \
+               f")"
